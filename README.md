40b1de8e8f4e732ae41e677672ae567dce291a36
Michael Hamilton <98mikeham@gmail.com>
2019-07-22 18:29:38 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# create-react-awesome-project CLI
+Setting up a react project with webpack, babel, jest/enzyme, redux and routing can be tricky.
+This project is a simplified version of create-react-app with the minimal dependencies to get a project up and running.
+
+## CLI
+```
+create-react-awesome-project
+```
+or
+```
+crap
+```
+
+### Options
+You can pass in the following options to do the following
+
+|CLI Options | Description |
+|--git | Initialize a git repository |
+|--install | Installs dependancies |
--- src/cli.js
+++ src/cli.js
@@ -5,27 +5,19 @@ import {createReactAwesomeApp} from './main';
 function parseArgumentsIntoOptions(rawArgs) {
     const args = arg({
         '--git': Boolean,
-        '--yes': Boolean,
         '--install': Boolean,
         '-g': '--git',
-        '-y': '--yes',
         '-i': '--install',
     }, {
         argv: rawArgs.slice(2)
     });
     return {
-        skipPrompts: args['--yes'] || false,
         git: args['--git'] || false,
-        // template: args._[0],
         runInstall: args['--install'] || false
     }
 }
 
 async function promptForMissingOptions(options) {
-    if(options.skipPrompts) {
-        return options
-    }
-
     const questions = [];
 
     questions.push({
